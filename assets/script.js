46326cc55eedc6f9ae6ca08408465ebf5f5c4f76
lucasBRYG <adio.lucas@gmail.com>
2020-09-24 11:00:05 -0400

    why are you not working???

--- assets/script.js
+++ assets/script.js
@@ -26,18 +26,20 @@ $(document).ready(function(){
     for(var i = 0; i < time.length; i ++){
         var timeDiv = $(time[i]);
         var militTime = timeDiv.attr("id");
-
-        toDo.push(JSON.parse(localStorage.getItem("hour-" + militTime)));
         var currentToDoDiv = timeDiv.children("textarea");
         var currentToDoObject = localStorage.getItem("hour-" + militTime);
-        currentToDoDiv.text(currentToDoObject.task);
-        if (militTime == currentMilitaryTime) {
+        
+        toDo[i] = currentToDoObject;
+        
+        currentToDoDiv.val(currentToDoObject.text);
+
+        if (parseInt(militTime) == parseInt(currentMilitaryTime)) {
             timeDiv.addClass("present").removeClass("past future");
         }
-        if (militTime < currentMilitaryTime) {
+        if (parseInt(militTime) < parseInt(currentMilitaryTime)) {
             timeDiv.addClass("past").removeClass("present future");
         }
-        if ((militTime > currentMilitaryTime)) {
+        if (parseInt(militTime) > parseInt(currentMilitaryTime)) {
             timeDiv.addClass("future").removeClass("past present");
         }
     }
@@ -45,8 +47,11 @@ $(document).ready(function(){
     schedule.find("button").on("click", function(){
         var currentToDoDiv = $(this).parent();
         var taskUpdate = currentToDoDiv.find("textarea").val().trim();
-        
-        
+        var militTime = $(currentToDoDiv).attr("id");
+        var toDoUpdate = 
+
+        toDo[parseInt(militTime) - 9].text = taskUpdate;
+        localStorage.setItem("hour-" + militTime, JSON.stringify(toDo[parseInt(militTime) - 9]))
     });
 });
 
